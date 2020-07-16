2a9152c1edd639c3870f097ed33018ec237f442f
Sebastian <sebastiamerelor@gmail.com>
2020-07-17 01:57:31 +0200

    Added grid with tiles

---
+++ Puzzle/Assets/Resources/GridMap/Scripts/Room.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    private Grid grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new Grid(5, 5, 1f, new Vector3(0, 0, 0), this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
