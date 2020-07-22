56fdccf8b9a0f1d373719166e2d302d5d097a509
Tako Eater <sexyexoss@gmail.com>
2020-07-22 03:49:46 +0200

    Fully working LevelSelection, still need some polish

--- Puzzle/Assets/LevelSelection.cs
+++ Puzzle/Assets/LevelSelection.cs
@@ -5,20 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelection : MonoBehaviour
 {
+    // Gameobject array to get reference of all the Coins in the scene
     private GameObject[] coins;
 
     void Update()
     {
+        // check if we are not in the menu
         if (SceneManager.GetActiveScene().buildIndex != 0)
         {
+            // find all the gameobjects with the tag "coin" then load the next scene when they are no more
             coins = GameObject.FindGameObjectsWithTag("Coin");
             if (coins.Length == 0)
             {
                 if (SceneManager.GetActiveScene().buildIndex + 1 < SceneManager.sceneCountInBuildSettings)
                     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                //if it went through all the levels available, redirects to Menu (build index 0)
+                else
+                    SceneManager.LoadScene(0);
+
             }
         }
     }
+
+    // Functions to load the levels based on the build index(Go to File->BuildSettings to manage it)
 
     public void GotoLevel1()
     {
@@ -40,5 +49,26 @@ public class LevelSelection : MonoBehaviour
     public void GotoLevel5()
     {
         SceneManager.LoadScene(5);
+    }
+
+    public void GotoLevel6()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void GotoLevel7()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void GotoLevel8()
+    {
+        SceneManager.LoadScene(8);
+    }
+    public void GotoLevel9()
+    {
+        SceneManager.LoadScene(9);
+    }
+    public void GotoLevel10()
+    {
+        SceneManager.LoadScene(10);
     }
 }
