be52262bbd02457932b344b91cd2c7f2d44cc602
Tako Eater <sexyexoss@gmail.com>
2020-07-22 03:30:47 +0200

    Added LevelSelection Canvas

--- Puzzle/Assets/LevelSelection.cs
+++ Puzzle/Assets/LevelSelection.cs
@@ -6,17 +6,39 @@ using UnityEngine.SceneManagement;
 public class LevelSelection : MonoBehaviour
 {
     private GameObject[] coins;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+
     void Update()
     {
-        coins = GameObject.FindGameObjectsWithTag("Coin");
-        if (coins.Length == 0)
+        if (SceneManager.GetActiveScene().buildIndex != 0)
         {
-            if (SceneManager.GetActiveScene().buildIndex < Application.levelCount)
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            coins = GameObject.FindGameObjectsWithTag("Coin");
+            if (coins.Length == 0)
+            {
+                if (SceneManager.GetActiveScene().buildIndex + 1 < SceneManager.sceneCountInBuildSettings)
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
         }
+    }
+
+    public void GotoLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GotoLevel2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void GotoLevel3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void GotoLevel4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void GotoLevel5()
+    {
+        SceneManager.LoadScene(5);
     }
 }
