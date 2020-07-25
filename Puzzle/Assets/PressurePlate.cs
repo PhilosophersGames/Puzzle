5a4680aa87720cb6e06a41ff777df0350bf0baf3
Bouzgaou <youcefbouzgaou@gmail.com>
2020-07-25 09:49:10 +0200

    Pressure Plates Fixes

--- Puzzle/Assets/PressurePlate.cs
+++ Puzzle/Assets/PressurePlate.cs
@@ -5,26 +5,35 @@ using UnityEngine;
 public class PressurePlate : MonoBehaviour
 {
     public GameObject door;
-    private SpriteRenderer sprite;
+    private SpriteRenderer spriteRenderer;
+    public Sprite spriteOn;
+    public Sprite spriteOff;
     // Start is called before the first frame update
     void Start()
     {
-        sprite = this.GetComponent<SpriteRenderer>();
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D other) {
         {
-            if (other.CompareTag("Object"))
+            if (other.CompareTag("Object") || other.CompareTag("Player"))
             {
+                spriteRenderer.sprite = spriteOn;
                 door.GetComponent<DoorAndKey>().OpenDoor();
             }
+         /*   else
+            {
+                spriteRenderer.sprite = spriteOff;
+                door.GetComponent<DoorAndKey>().CloseDoor();
+            }*/
         }
     }
         private void OnTriggerExit2D(Collider2D other) {
         {
-            if (other.CompareTag("Object"))
+            if ((other.CompareTag("Object") || other.CompareTag("Player")))
             {
+                spriteRenderer.sprite = spriteOff;
                 door.GetComponent<DoorAndKey>().CloseDoor();
             }
         }
