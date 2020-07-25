b916173495063bcedf5fe3a409977b40d83b0ccb
Tako Eater <sexyexoss@gmail.com>
2020-07-25 08:04:32 +0200

    PressurePlate added

---
+++ Puzzle/Assets/PressurePlate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public GameObject door;
+    private SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other) {
+        {
+            if (other.CompareTag("Object"))
+            {
+                door.GetComponent<DoorAndKey>().OpenDoor();
+            }
+        }
+    }
+        private void OnTriggerExit2D(Collider2D other) {
+        {
+            if (other.CompareTag("Object"))
+            {
+                door.GetComponent<DoorAndKey>().CloseDoor();
+            }
+        }
+    }
+}
