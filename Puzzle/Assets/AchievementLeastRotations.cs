2a017617f2bd6d3642501c68bf48266efe8a0a41
Tako Eater <sexyexoss@gmail.com>
2020-07-29 04:59:17 +0200

    V.02 hotfix

--- Puzzle/Assets/AchievementLeastRotations.cs
+++ Puzzle/Assets/AchievementLeastRotations.cs
@@ -7,14 +7,19 @@ public class AchievementLeastRotations : MonoBehaviour
    public bool achievement = false;
    public int rotationNumbers = 0;
 
+   private bool multipleCount = true;
+
     void Update() 
     {
        if (achievement == true)
        {
-           if (Input.GetKeyDown("e") || Input.GetKeyDown("r"))
-           {
-               rotationNumbers++;
-           }        
+         if (RoomTransition.isRotating == true && multipleCount == true)
+         {
+            rotationNumbers++;
+            multipleCount = false;
+         }
+         if (RoomTransition.isRotating == false)
+            multipleCount = true;
         }
     }
 }
