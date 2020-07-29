2e15937899b2917afffb30338139267a47e58881
Tako Eater <sexyexoss@gmail.com>
2020-07-29 05:24:06 +0200

    movement true

--- Puzzle/Assets/Resources/Scripts/Movement.cs
+++ Puzzle/Assets/Resources/Scripts/Movement.cs
@@ -6,12 +6,9 @@ public class Movement : MonoBehaviour
     public Animator animator;
     public Rigidbody2D rb;
     public float moveSpeed;
-
-    private bool rotatePlayer;
-
+    private bool rotatePlayer = true;
     void Update()
     {
-
         //transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 0f);
         if (RoomTransition.isRotating == false)
             rb.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), moveSpeed * Input.GetAxis("Vertical"));
