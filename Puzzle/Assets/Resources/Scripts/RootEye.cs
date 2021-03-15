5619556b753553a104207ec83dfc3b18f474e152
Miguel <sexyexoss@gmail.com>
2021-03-15 17:09:36 +0100

    blueye

--- Puzzle/Assets/Resources/Scripts/RootEye.cs
+++ Puzzle/Assets/Resources/Scripts/RootEye.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class freezingEye : MonoBehaviour
+public class RootEye : MonoBehaviour
 {
 
     // Start is called before the first frame update
 
-
     private LineRenderer lineRenderer;
+    private float tempMoveSpeed;
 
     private GameObject analogues;
     private GameObject Target;
@@ -20,9 +20,9 @@ public class freezingEye : MonoBehaviour
         Target = GameObject.FindWithTag("Player");
     }
 
-
     private void Start()
     {
+       // Physics.IgnoreCollision(Target.GetComponent<Collider>(), GetComponent<Collider>());
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.enabled = false;
         analogues = GameObject.Find("Rotation Buttons");
@@ -35,22 +35,18 @@ public class freezingEye : MonoBehaviour
         // look at the target, cast a raycast, and constraint rotation if the player is inSight
         Vector3 dir = Target.transform.position - transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-
-
         Debug.DrawRay(transform.position, Target.transform.position - transform.position, Color.red);
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Target.transform.position - transform.position, Mathf.Infinity, ~layerMaskTab);
-
         // SET the linerenderer position on sight
         lineRenderer.SetPosition(0, transform.position);
         lineRenderer.SetPosition(1, Target.transform.position);
 
-
         if (hit.collider.tag == "Player" && inSight == false)
         {
-
+            tempMoveSpeed = hit.collider.transform.GetComponent<Movement>().moveSpeed;
             tempPlayer = hit.collider.gameObject;
             inSight = true;
-            hit.collider.transform.parent.GetComponentInChildren<RoomTransition>().canRotate = false;
+            hit.collider.transform.GetComponent<Movement>().moveSpeed = 0;
             analogues.SetActive(false);
             Debug.Log("Player In sight");
         }
@@ -59,7 +55,7 @@ public class freezingEye : MonoBehaviour
             inSight = false;
             lineRenderer.enabled = false;
             analogues.SetActive(true);
-            tempPlayer.transform.parent.GetComponentInChildren<RoomTransition>().canRotate = true;
+            tempPlayer.GetComponent<Movement>().moveSpeed = tempMoveSpeed;
         }
         if (inSight == true)
         {
@@ -67,8 +63,7 @@ public class freezingEye : MonoBehaviour
             lineRenderer.enabled = true;
             tempPlayer = hit.collider.gameObject;
             analogues.SetActive(false);
-            tempPlayer.transform.parent.GetComponentInChildren<RoomTransition>().canRotate = false;
+            hit.collider.transform.GetComponent<Movement>().moveSpeed = 0;
         }
     }
 }
-
