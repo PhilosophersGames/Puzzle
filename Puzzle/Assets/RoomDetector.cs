f75f8b1ae9b063c3e59ec651816d7e212cf763f1
Miguel <sexyexoss@gmail.com>
2021-03-22 21:41:41 +0100

    room colliders

--- Puzzle/Assets/RoomDetector.cs
+++ Puzzle/Assets/RoomDetector.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class RoomDetector : MonoBehaviour
 {
     public GameObject room;
+    public Collider2D col;
+
+   void Awake()
+    {
+            col = GetComponentInChildren<BoxCollider2D>();
+    }
     
+    void Update() {
+        if (!room && !col.enabled)
+            col.enabled = true;
+    }
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.CompareTag("AdjacentRooms"))
