f2c36341da9894ac6b9caac27fb478f51ba4b8bb
Miguel <sexyexoss@gmail.com>
2021-05-24 15:06:09 +0200

    UIShowAnimations

--- Puzzle/Assets/Resources/Scripts/ColorPaletteAnimation.cs
+++ Puzzle/Assets/Resources/Scripts/ColorPaletteAnimation.cs
@@ -4,29 +4,45 @@ using UnityEngine;
 
 public class ColorPaletteAnimation : MonoBehaviour
 {
-   [SerializeField] private Animator anim;
+   [SerializeField] private Animator[] anim;
 
-    public GameObject panel;
+   [SerializeField] private Animator showUIButtonAnim;
+
+   [SerializeField] private GameObject showUIButton;
+
+    [SerializeField] private GameObject pauseButton;
+
+   [SerializeField] private GameObject[] panel;
 
 	private bool firstUpdateFrame = true;
 
     public void LateUpdate()
     {
-        if(panel.active && firstUpdateFrame)
+        if(panel[0].active && firstUpdateFrame)
 		{
-            panel.SetActive(false);
+            panel[0].SetActive(false);
+            panel[1].SetActive(false);
 			firstUpdateFrame = false;
 		}
     }
 
     public void ActivePanel()
     {
-        if (panel.active)
-            anim.SetTrigger("ClosePanel");
+        if (panel[0].active)
+        {
+            showUIButtonAnim.SetTrigger("HideUI");
+            pauseButton.SetActive(false);
+            anim[0].SetTrigger("ClosePanel");
+            anim[1].SetTrigger("ClosePanel");
+        }
         else
         {
-			panel.SetActive(true);
-            anim.SetTrigger("OpenPanel");
+            showUIButtonAnim.SetTrigger("ShowUI");           
+            pauseButton.SetActive(true);
+			panel[0].SetActive(true);
+            anim[0].SetTrigger("OpenPanel");
+            panel[1].SetActive(true);
+            anim[1].SetTrigger("OpenPanel");
         }
     }
 }
