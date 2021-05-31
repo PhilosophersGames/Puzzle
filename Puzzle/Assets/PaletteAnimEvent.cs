1a0dfbb42b3e254afb640dc53e61fcac60fca375
Miguel <sexyexoss@gmail.com>
2021-05-31 23:21:28 +0200

    PaletteTile

--- Puzzle/Assets/PaletteAnimEvent.cs
+++ Puzzle/Assets/PaletteAnimEvent.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class PaletteAnimEvent : MonoBehaviour
 {
+
+    [SerializeField] private GameObject switchPaletteButton;
+    
     public void DisablePanel()
     {
         transform.parent.gameObject.SetActive(false);
+    }
+
+    public void EnableSwitchPaletteButton()
+    {
+        switchPaletteButton.SetActive(true);
+    }
+
+    public void DisableSwitchPaletteButton()
+    {
+        switchPaletteButton.SetActive(false);
     }
 }
