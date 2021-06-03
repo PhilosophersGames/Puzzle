d2b80a058b41476af6ba088ad9f69e4c0ae062cc
Miguel <sexyexoss@gmail.com>
2021-06-03 17:16:15 +0200

    WIP PaletteAnimation

--- Puzzle/Assets/Resources/Scripts/ColorPaletteAnimation.cs
+++ Puzzle/Assets/Resources/Scripts/ColorPaletteAnimation.cs
@@ -4,21 +4,13 @@ using UnityEngine;
 
 public class ColorPaletteAnimation : MonoBehaviour
 {
-   [SerializeField] private Animator[] anim;
-
-   [SerializeField] private Animator showUIButtonAnim;
-
-   [SerializeField] private GameObject showUIButton;
-
-   [SerializeField] private GameObject rotationButton;
-
-    [SerializeField] private GameObject pauseButton;
-
    [SerializeField] private GameObject[] panel;
-
+   [SerializeField] private GameObject panelManager;
+   [SerializeField] private Animator panelManagerAnim;
+                                                                                                                                                             
     private bool firstUpdateFrame = true;
 
-    public void LateUpdate()
+    /*public void LateUpdate()
     {
         if(panel[0].active && firstUpdateFrame)
 		{
@@ -26,32 +18,15 @@ public class ColorPaletteAnimation : MonoBehaviour
             panel[1].SetActive(false);
 			firstUpdateFrame = false;
 		}
+    }*/
+
+    public void OpenPanel()
+    {
+        panelManagerAnim.SetTrigger("OpenPanel");
     }
 
-    public void ActivePanel()
+    public void ClosePanel()
     {
-        if (panel[0].active)
-        {
-            if (panel[2].active)
-            {
-                panel[2].SetActive(false);
-                panel[1].transform.parent.gameObject.SetActive(true);
-            }
-            rotationButton.SetActive(true);
-            showUIButtonAnim.SetTrigger("HideUI");
-            pauseButton.SetActive(false);
-            anim[0].SetTrigger("ClosePanel");
-            anim[1].SetTrigger("ClosePanel");
-        }
-        else
-        {
-            rotationButton.SetActive(false);
-            showUIButtonAnim.SetTrigger("ShowUI");           
-            pauseButton.SetActive(true);
-			panel[0].SetActive(true);
-            anim[0].SetTrigger("OpenPanel");
-            panel[1].SetActive(true);
-            anim[1].SetTrigger("OpenPanel");
-        }
+            panelManagerAnim.SetTrigger("ClosePanel");
     }
 }
